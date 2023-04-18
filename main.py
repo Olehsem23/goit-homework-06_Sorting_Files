94349c09a232e70eb707e2d2ab8ff58d3f3d50b2
Olehsem23 <olehsem23@gmail.com>
2023-04-18 17:48:14 +0300

    Optimization of file
    All functions now are in one file. There is no need to have bigger number of files.

--- main.py
+++ main.py
@@ -1,96 +1,118 @@
-'''
-Завдання: Сортування файлів у папці.
-Копіювати файли із зазначеної папки та покласти в нову папку з розширенням цього файлу.
-'''
-import argparse
 from pathlib import Path
 import shutil
+import re
 import sys
-import file_parser as parser
-from normalize import normalize
+
+CYRILLIC_SYMBOLS = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ'
+TRANSLATION = ("a", "b", "v", "g", "d", "e", "yo", "zh", "z", "y", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
+               "f", "h", "c", "ch", "sh", "sch", "", "y", "", "e", "yu", "ya", "ye", 'i', "yi", "g")
+print(len(CYRILLIC_SYMBOLS))
+print(len(TRANSLATION))
+
+TRANS = {}
+for c, l in zip(CYRILLIC_SYMBOLS, TRANSLATION):
+    TRANS[ord(c)] = l
+    TRANS[ord(c.upper())] = l.upper()
+
+KN_EXT = set()
+UN_EXT = set()
+
+EXTENSIONS = {
+    'images': ['JPEG', 'PNG', 'JPG', 'SVG'],
+    'video': ['AVI', 'MP4', 'MOV', 'MKV'],
+    'documents': ['DOC', 'DOCX', 'TXT', 'PDF', 'XLS', 'XLSX', 'PPTX'],
+    'audio': ['MP3', 'OGG', 'WAV', 'AMR'],
+    'archives': ['ZIP', 'GZ', 'TAR'],
+    'unknown_extensions': []
+}
+
+ALL_FILES = {
+    'images': [],
+    'video': [],
+    'documents': [],
+    'audio': [],
+    'archives': [],
+    'unknown_extensions': []
+}
 
 
-def handle_media(filename: Path, target_folder: Path) -> None:
-    target_folder.mkdir(exist_ok=True, parents=True)
-    filename.replace(target_folder / normalize(filename.name))
+def scan_folder(path_to_folder: Path) -> None:  # Функція сканує папку на наявність файлів і вкладених папок
+    for element in path_to_folder.iterdir():
+        if element.is_dir() and element in EXTENSIONS.keys():
+            continue
+        elif element.is_dir():
+            if any(element.iterdir()):
+                scan_folder(element)  # Рекурсивно проходимо по всіх вкладених папках
+            else:
+                element.rmdir()  # Видаляємо порожні папки
+        else:
+            sort_file(element)
+            handle_file(element)
+            if not any(element.parent.iterdir()): element.parent.rmdir()  # Роздаємо задачі іншим функціям
 
-def handle_other(filename: Path, target_folder: Path) -> None:
-    target_folder.mkdir(target_folder / normalize(filename.name))
-    filename.replace(target_folder / normalize(filename.name))
 
-def handle_archive(filename: Path, target_folder: Path) -> None:
-    target_folder.mkdir(exist_ok=True, parents=True)
-    folder_for_file = target_folder / normalize((filename.name))
-    folder_for_file.mkdir(exist_ok=True, parents=True)
+def unpack_archive(path_to_archive: Path, folder: str) -> None:  # Розпакування архівів
+    new_folder = Path(path_to_folder) / folder
+    if not new_folder.exists():
+        new_folder.mkdir(exist_ok=True, parents=True)
+    ALL_FILES[folder].append(normalize_name(path_to_archive))
+    shutil.unpack_archive(path_to_archive, new_folder / normalize_name(path_to_archive).split('.')[0])
+    Path.unlink(path_to_archive)  # Видалення архіву після розпакування
+
+
+def normalize_name(path_to_file: Path) -> str:  # Транслітерація назв файлів
+    name = path_to_file.name.split('.')
+    name[0] = re.sub(r'\W', '_', name[0].translate(TRANS))
+    new_name = '.'.join(name)
+    return new_name
+
+
+def handle_file(path_to_file: Path) -> None:  # Створення списків по групам файлів
+    ext = path_to_file.suffix[1:].upper()
+    for key, value in EXTENSIONS.items():
+        if ext in value:
+            ALL_FILES[key].append(normalize_name(path_to_file))
+            return
+    ALL_FILES['unknown_extensions'].append(normalize_name(path_to_file))
+
+
+def sort_file(path_to_file: Path) -> list:  # Сортування файлів за розширенням та тип
+    ext = path_to_file.suffix[1:].upper()
+    for key, value in EXTENSIONS.items():
+        if ext in value:
+            KN_EXT.add(ext)
+            if key == 'archives':
+                unpack_archive(path_to_file, key)
+                return
+            else:
+                move_file(path_to_file, key)
+                return
+    UN_EXT.add(ext)
+    move_file(path_to_file, 'unknown_extensions')
+
+
+def move_file(path_to_file: Path, folder: str) -> None:  # Переносимо файли у папки відповідного вмісту
+    new_folder = Path(path_to_folder) / folder
+    if not new_folder.exists():
+        new_folder.mkdir(exist_ok=True, parents=True)
     try:
-        shutil.unpack_archive(str(filename.resolve()), str(folder_for_file.resolve()))
-    except shutil.ReadError:
-        folder_for_file.rmdir()
-        return None
-    filename.unlink()
+        shutil.copyfile(path_to_file, new_folder / Path(normalize_name(path_to_file)))
+        Path.unlink(path_to_file)
+    except:
+        print(f'Can`t move this file: {normalize_name(path_to_file)}!')
 
-def handle_folder(folder: Path) -> None:
-    try:
-        folder.rmdir()
-    except OSError:
-        print(f'Sorry, we can not delete the folder: {folder}')
-
-def main(folder: Path) -> None:
-    parser.scan(folder)
-    for file in parser.JPEG_IMAGES:
-        handle_media(file, folder / 'images' / 'JPEG')
-    for file in parser.PNG_IMAGES:
-        handle_media(file, folder / 'images' / 'PNG')
-    for file in parser.JPG_IMAGES:
-        handle_media(file, folder / 'images' / 'JPG')
-    for file in parser.SVG_IMAGES:
-        handle_media(file, folder / 'images' / 'SVG')
-    for file in parser.AVI_VIDEO:
-        handle_media(file, folder / 'video' / 'AVI')
-    for file in parser.MP4_VIDEO:
-        handle_media(file, folder / 'video' / 'MP4')
-    for file in parser.MOV_VIDEO:
-        handle_media(file, folder / 'video' / 'MOV')
-    for file in parser.MKV_VIDEO:
-        handle_media(file, folder / 'video' / 'MKV')
-    for file in parser.DOC_DOCUMENTS:
-        handle_media(file, folder / 'documents' / 'DOC')
-    for file in parser.DOCX_DOCUMENTS:
-        handle_media(file, folder / 'documents' / 'DOCX')
-    for file in parser.TXT_DOCUMENTS:
-        handle_media(file, folder / 'documents' / 'TXT')
-    for file in parser.PDF_DOCUMENTS:
-        handle_media(file, folder / 'documents' / 'PDF')
-    for file in parser.XLS_DOCUMENTS:
-        handle_media(file, folder / 'documents' / 'XLS')
-    for file in parser.XLSX_DOCUMENTS:
-        handle_media(file, folder / 'documents' / 'XLSX')
-    for file in parser.PPTX_DOCUMENTS:
-        handle_media(file, folder / 'documents' / 'PPTX')
-    for file in parser.MP3_AUDIO:
-        handle_media(file, folder / 'music' / 'MP3')
-    for file in parser.OGG_AUDIO:
-        handle_media(file, folder / 'music' / 'OGG')
-    for file in parser.WAV_AUDIO:
-        handle_media(file, folder / 'music' / 'WAV')
-    for file in parser.AMR_AUDIO:
-        handle_media(file, folder / 'music' / 'AMR')
-
-    for file in parser.ZIP_ARCHIVES:
-        handle_media(file, folder / 'archives' / 'zip')
-    for file in parser.GZ_ARCHIVES:
-        handle_media(file, folder / 'archives' / 'gz')
-    for file in parser.TAR_ARCHIVES:
-        handle_media(file, folder / 'archives' / 'tar')
-
-    for file in parser.MY_OTHER:
-        handle_media(file, folder / 'MY_OTHER')
-
-    for folder in parser.FOLDERS[::-1]:
-        handle_folder(folder)
 
 if __name__ == '__main__':
-    folder_for_scan = Path(sys.argv[1])
-    main(folder_for_scan.resolve())
+    path_to_folder = sys.argv[1]
 
+    scan_folder(Path(path_to_folder))
 
+    print(f"Images: {' ** '.join(ALL_FILES['images'])}\n")
+    print(f"Video: {' ** '.join(ALL_FILES['video'])}\n")
+    print(f"Documents: {' ** '.join(ALL_FILES['documents'])}\n")
+    print(f"Audio: {' ** '.join(ALL_FILES['audio'])}\n")
+    print(f"Archives: {' ** '.join(ALL_FILES['archives'])}\n")
+    print(f"Unknown_extensions: {' ** '.join(ALL_FILES['unknown_extensions'])}\n")
+    print('*' * 60)
+    print(f'\nUNKNOWN EXTENSIONS: {UN_EXT}\n')
+    print(f'KNOWN EXTENSIONS: {KN_EXT}')
